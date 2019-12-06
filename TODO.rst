2d4e6f92c0e87c1a0e4d0c5e8f8007129505a994
seppowalther <seppo.walther@educorvi.de>
2019-12-06 11:14:00 +0100

    Added content type documentation and added what is still to do and what doesn't work yet in TODO.rst

--- TODO.rst
+++ TODO.rst
@@ -3,3 +3,10 @@ TO-DOS
 ======
 
 What to do / What isn't working yet:
+
+****************
+dokumentation.py
+****************
+- Widgets (RichText?)
+- genWebcode
+- ObjPathSourceBinder
--- src/uvc/plonedokumentation/content/dokumentation.py
+++ src/uvc/plonedokumentation/content/dokumentation.py
@@ -1,61 +1,134 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
+from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
+from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 
-# from uvc.plonedokumentation import _
+from uvc.plonedokumentation import _
 
 
 class IDokumentation(model.Schema):
-    """ Marker interface and Dexterity Python Schema for Dokumentation
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    Artikeltyp zur Erfassung von Dokumentationen im Bereich Praevention der BG ETEM.
+    """
 
-    # model.load('dokumentation.xml')
+#    model.fieldset(
+#            'doku_details',
+#            label=_(u'gut zu wissen'),
+#            fields=('details',),
+#    )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+#    model.fieldset('doku_zusatzinfos',
+#            label=_(u'weiss nicht jeder'),
+#            fields=('zusatzinfos',),
+#            )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+#    model.fieldset(
+#            'doku_bilder',
+#            label=_(u'Bilder'),
+#            fields=('titelbilder', 'bilder','nachrichtenbild','bildtitel'),
+#            )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+#    model.fieldset(
+#            'doku_dateien_links',
+#            label=_(u'Dateien/Links'),
+#            fields=('dateien','links'),
+#            )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    ueberschrift = schema.TextLine(title=_(u"Überschrift"),
+                   description=_(u"Hier können Sie dem Dokument eine Überschrift geben und damit den Title überschrieben."),
+                   required = False,)
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    haupttext = RichText(
+                title=_(u"wichtig zu wissen"),
+                description=_(u"(grundlegende Informationen zum Thema - Haupttext)"),
+                required = True,
+                )
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    details = RichText(
+              title=_(u"gut zu wissen"),
+              description=_(u"(detaillierte Informationen ergänzend zu den Grundlagen)"),
+              required = False,
+              )
 
+    zusatzinfos = RichText(
+              title=_(u"weiss nicht jeder"),
+              description=_(u"(Interessantes und Wissenswertes, ergänzend zu den Grundlagen und Details)"),
+              required = False,
+              )
+
+#    titelbilder = RelationList(title=_(u"Titelbilder"),
+#                           description=_(u"(Anzeige im Kopf der Seite)"),
+#                           default=[],
+#                           value_type=RelationChoice(title=_(u"Titelbilder"),
+#                                                     source=ObjPathSourceBinder()),
+#                           required=False,)
+
+#    bilder = RelationList(
+#              title=_(u"Illustration"),
+#              description=_(u"(Bilder zur Anzeige unterhalb der Inhaltstexte)"),
+#              value_type=RelationChoice(title=_(u"Bilder"), 
+#                                        source=ObjPathSourceBinder()),
+#              required=False,
+#              )
+
+#    webcode = schema.TextLine(
+#              title=_(u"Webcode"),
+#              description=_(u"Der Webcode für diesen Artikel wird automatisch errechnet und angezeigt. Sie\
+#                          können diesen Webcode bei Bedarf jedoch jederzeit überschreiben."),
+#              required = True,
+#              defaultFactory = genWebcode,
+#              )
+
+    vorgaenger = schema.TextLine(
+              title=_(u"Vorgänger"),
+              description=_(u"Bitte tragen Sie den Webcode des Vorgängerdokuments ein."),
+              required = False,
+              )
+
+    nachfolger = schema.TextLine(
+              title=_(u"Nachfolger"),
+              description=_(u"Bitte tragen Sie den Webcode des Nachfolgerdokuments ein."),
+              required = False,
+              )
+
+#    weiterlesen = RelationList(title=_(u"Dokumente zum Weiterlesen"),
+#                           description=_(u"Bitte wählen Sie hier Dokumente aus, die Sie dem Benutzer zum\
+#                                   Weiterlesen empfehlen."),
+#                           default=[],
+#                           value_type=RelationChoice(title=_(u"Weiterlesen"),
+#                                                     source=ObjPathSourceBinder()),
+#                           required=False,)
+
+#    nachrichtenbild = RelationChoice(
+#        title=_(u"Vorschaubild"),
+#        description=_(u"(Anzeige in Verweisboxen oder Ordneransichten)"),
+#        source=ObjPathSourceBinder(),
+#        required=False,
+#        )
+
+    bildtitel = schema.TextLine(
+              title=_(u"Titel des Vorschaubildes"),
+              required = False,
+              )
+
+#    directivesform.widget(dateien=MultiFileFieldWidget)
+#    dateien = schema.List(title=_(u'Ihre Dateien für dieses Dokument'),
+#                             value_type=NamedBlobFile(),
+#                             required = False,)
+
+#    form.widget(links=DataGridFieldFactory)
+#    links = schema.List(title=_(u'Ihre Links für dieses Dokument'),
+#                        value_type=DictRow(title=_(u"Link"), schema=ILink),
+#                        required = False,)
 
 @implementer(IDokumentation)
 class Dokumentation(Container):
